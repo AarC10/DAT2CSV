2b6342f9bd5cb2f5017121cd971b981136793108
Aaron Chan <aarchan108@gmail.com>
2022-10-20 10:56:33 -0400

    Use try with resources now. Apparently only file path is being written as the data for some reason

--- src/main/java/apps/DATConZipProcessor.java
+++ src/main/java/apps/DATConZipProcessor.java
@@ -7,35 +7,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
 public class DATConZipProcessor {
-    public static void main(String[] args) throws NotDatFile, IOException, FileEnd {
-        String datFolderLocation = args[0];
-        File datFolder = new File(datFolderLocation);
-
-        String zipFolderLocation = args[1];
-        File zipFolder = new File(zipFolderLocation);
-        ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(zipFolder));
-
-
-        for (File file : Objects.requireNonNull(datFolder.listFiles())) {
-            if (file.isFile() && file.getName().endsWith(".DAT")) {
-                processDATFile(file);
-                File convertedFile = new File(file.getAbsolutePath() + ".csv");
-                if (convertedFile.exists()) {
-                    zipOutputStream.putNextEntry(new ZipEntry(convertedFile.getName()));
-                    zipOutputStream.write(convertedFile.getAbsolutePath().getBytes());
-                    zipOutputStream.closeEntry();
-                }
-            }
-        }
-
-        zipOutputStream.close();
-    }
-
-
     private static void processDATFile(File file) throws NotDatFile, IOException, FileEnd {
         System.out.println("Processing: " + file.getAbsolutePath());
         DatFile datFile = DatFile.createDatFile(file.getAbsolutePath());
@@ -51,5 +25,30 @@ public class DATConZipProcessor {
 
         datFile.reset();
         AnalyzeDatResults results = convertDat.analyze(true);
+    }
+
+    public static void main(String[] args) throws NotDatFile, IOException, FileEnd {
+        String datFolderLocation = args[0];
+        File datFolder = new File(datFolderLocation);
+
+        String zipFolderLocation = args[1];
+        File zipFolder = new File(zipFolderLocation);
+
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(zipFolder))) {
+
+
+            for (File file : Objects.requireNonNull(datFolder.listFiles())) {
+                if (file.isFile() && file.getName().endsWith(".DAT")) {
+                    processDATFile(file);
+                    File convertedFile = new File(file.getAbsolutePath() + ".csv");
+                    if (convertedFile.exists()) {
+                        zipOutputStream.putNextEntry(new ZipEntry(convertedFile.getName()));
+                        zipOutputStream.write(convertedFile.getAbsolutePath().getBytes());
+                        zipOutputStream.closeEntry();
+                    }
+                }
+            }
+        }
+
     }
 }
