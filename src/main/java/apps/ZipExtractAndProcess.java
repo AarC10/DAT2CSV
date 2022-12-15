f3f6e9dc3c8fd6cd6b2c16d92bd8f34549079096
Aaron Chan <aarchan108@gmail.com>
2022-12-14 23:34:48 -0500

    Try with resources and a little more cleanup of test code

--- src/main/java/apps/ZipExtractAndProcess.java
+++ src/main/java/apps/ZipExtractAndProcess.java
@@ -13,18 +13,22 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
 public class ZipExtractAndProcess {
-    private static void placeInZip(String file) throws IOException {
+    private static void placeInZip(String file, String zipFileName) throws IOException {
         System.out.println("Placing " + file + " in zip");
+
         Map<String, String> env = new HashMap<>();
         env.put("create", "true");
 
-        Path path = Paths.get(Paths.get(file).getParent() + "/DJIData.zip");
+        Path filePath = Paths.get(file);
+        Path path = Paths.get(filePath.getParent() + "/" + zipFileName);
+
         URI uri = URI.create("jar:" + path.toUri());
-        try (FileSystem fs = FileSystems.newFileSystem(uri, env)) {
-            Path nf = fs.getPath(file.substring(file.lastIndexOf("/") + 1));
-            Files.write(nf, Files.readAllBytes(Paths.get(file)), StandardOpenOption.CREATE);
+        try (FileSystem fileSystem = FileSystems.newFileSystem(uri, env)) {
+            Path fileSystemPath = fileSystem.getPath(file.substring(file.lastIndexOf("/") + 1));
+            Files.write(fileSystemPath, Files.readAllBytes(filePath), StandardOpenOption.CREATE);
         }
     }
+
     private static File processDATFile(File file) throws NotDatFile, IOException, FileEnd {
         System.out.println("Processing: " + file.getAbsolutePath());
         DatFile datFile = DatFile.createDatFile(file.getAbsolutePath());
@@ -58,19 +62,19 @@ public class ZipExtractAndProcess {
 
             String zipName = zipEntry.getName().substring(zipEntry.getName().lastIndexOf("/"));
             File file = new File(parentFolder, zipName);
-            byte[] buffer = new byte[1024];
-            InputStream inputStream = zipFile.getInputStream(zipEntry);
-            FileOutputStream fileOutputStream = new FileOutputStream(file);
-            int len;
-            while ((len = inputStream.read(buffer)) > 0) {
-                fileOutputStream.write(buffer, 0, len);
+
+            try (InputStream inputStream = zipFile.getInputStream(zipEntry); FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+                int len;
+                byte[] buffer = new byte[1024];
+
+                while ((len = inputStream.read(buffer)) > 0) {
+                    fileOutputStream.write(buffer, 0, len);
+                }
             }
-            fileOutputStream.close();
-            inputStream.close();
 
             File datFile = processDATFile(file);
             File csvFile = new File(datFile.getAbsolutePath() + ".csv");
-            placeInZip(csvFile.getAbsolutePath());
+            placeInZip(csvFile.getAbsolutePath(), zipFile.getName().substring(zipFile.getName().lastIndexOf("/") + 1));
             csvFile.delete();
             file.delete();
         }
